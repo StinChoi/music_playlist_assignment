49b9405bb2e0bcf44fe0da3c46fbc2cc74084d35
Austin <austinchoi22@gmail.com>
2021-12-06 15:30:24 -0700

    playlists.js

--- src/Playlists.js
+++ src/Playlists.js
@@ -1,6 +1,36 @@
 import React from "react";
+import { Button, Table, } from "semantic-ui-react";
 
-const Playlists = (props) => {
+const Playlists = ({playlists}) => (
   //render data here
-  
-}
+  // using the first table in react.semantic
+  <Table celled padded>
+  <Table.Header>
+    <Table.Row>
+      <Table.HeaderCell>Artist</Table.HeaderCell>
+      <Table.HeaderCell>Album</Table.HeaderCell>
+      <Table.HeaderCell>Options</Table.HeaderCell>
+    </Table.Row>
+  </Table.Header>
+
+  <Table.Body>
+    { //code is breaking here
+      playlists.map( playlist => (    
+      <Table.Row key={playlist.id}>
+        <Table.Cell>{playlist.id}</Table.Cell>
+        <Table.Cell>{playlist.artist}</Table.Cell>
+        <Table.Cell>{playlist.album}</Table.Cell>
+        <Table.Cell>
+          <Button color="blue">
+            Delete
+          </Button>
+        </Table.Cell>
+      </Table.Row>
+      ))
+    } 
+    {/* code breaks */}
+    </Table.Body>
+    </Table>
+);
+
+export default Playlists;
